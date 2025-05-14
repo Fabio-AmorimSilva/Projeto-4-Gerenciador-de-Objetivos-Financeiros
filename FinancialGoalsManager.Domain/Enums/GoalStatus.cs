91d99b549125621c93bc63f1c2e01449f364cd01
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-05-14 19:59:14 -0300

    feat: creating enums

---
+++ FinancialGoalsManager.Domain/Enums/GoalStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace FinancialGoalsManager.Domain.Enums;
+
+public enum GoalStatus
+{
+    InProgress = 1,
+    Completed = 2,
+    Canceled = 3,
+    Paused = 4
+}
---
+++ FinancialGoalsManager.Domain/Enums/TransactionType.cs
@@ -0,0 +1,7 @@
+﻿namespace FinancialGoalsManager.Domain.Enums;
+
+public enum TransactionType
+{
+    Deposit = 1,
+    Withdraw = 2
+}

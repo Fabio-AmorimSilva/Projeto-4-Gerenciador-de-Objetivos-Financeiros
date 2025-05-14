a59918a8cc9c5d336e28b2478eb224e90cd713b6
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-05-14 19:59:04 -0300

    feat: Creating entities

---
+++ FinancialGoalsManager.Domain/Entities/FinancialGoal.cs
@@ -0,0 +1,30 @@
+﻿namespace FinancialGoalsManager.Domain.Entities;
+
+public sealed class FinancialGoal : SoftDeleteEntity
+{
+    public string Title { get; private set; } = null!;
+    public decimal Goal { get; private set; }
+    public DateTime DueDate { get; private set; }
+    public decimal? MonthGoal { get; private set; }
+    public GoalStatus Status { get; private set; }
+
+    private readonly List<Transaction> _transactions = [];
+    public IReadOnlyCollection<Transaction> Transactions => _transactions;
+
+    public FinancialGoal(){}
+    
+    public FinancialGoal(
+        string title,
+        decimal goal,
+        DateTime dueDate, 
+        decimal? monthGoal,
+        GoalStatus status
+    )
+    {
+        Title = title;
+        Goal = goal;
+        DueDate = dueDate;
+        MonthGoal = monthGoal;
+        Status = status;
+    }
+}
---
+++ FinancialGoalsManager.Domain/Entities/Transaction.cs
@@ -0,0 +1,21 @@
+﻿namespace FinancialGoalsManager.Domain.Entities;
+
+public sealed class Transaction : SoftDeleteEntity
+{
+    public decimal Quantity { get; private set; }
+    public TransactionType Type { get; private set; }
+    public DateTime Date { get; private set; }
+
+    public Transaction(){}
+    
+    public Transaction(
+        decimal quantity,
+        TransactionType type,
+        DateTime date
+    )
+    {
+        Quantity = quantity;
+        Type = type;
+        Date = date;
+    }
+}

da4fb1ec85185e55a25a3459f024fba9045ef478
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-05-15 19:50:53 -0300

    feat: Adding ErrorMessages class

---
+++ FinancialGoalsManager.Domain/Messages/ErrorMessages.cs
@@ -0,0 +1,19 @@
+﻿namespace FinancialGoalsManager.Domain.Messages;
+
+public static class ErrorMessages
+{
+    public static string CannotBeEmpty(string field)
+        => $"{field} cannot be empty.";
+    
+    public static string NotFound<T>()
+        => $"{typeof(T).Name} not Found.";
+
+    public static string HasMaxLength(string field, int maxLength)
+        => $"{field} cannot be more than {maxLength} characters.";
+    
+    public static string IsInvalidEnum(string field)
+        => $"{field} is not valid.";
+    
+    public static string HasToBeGreaterThan(string field, int value)
+        => $"{field} must be greater than {value}.";
+}

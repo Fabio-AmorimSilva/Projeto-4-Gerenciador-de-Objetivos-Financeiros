d89af692eb09fa39d8b866c578e5db0a5d5b7b20
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-05-15 19:53:01 -0300

    feat: Adding Community.Toolkit package in Domain

--- FinancialGoalsManager.Domain/GlobalUsings.cs
+++ FinancialGoalsManager.Domain/GlobalUsings.cs
@@ -1,2 +1,5 @@
-﻿global using FinancialGoalsManager.Domain.Entities.Common;
+﻿global using CommunityToolkit.Diagnostics;
+global using FinancialGoalsManager.Domain.Entities.Common;
 global using FinancialGoalsManager.Domain.Enums;
+global using FinancialGoalsManager.Domain.Exceptions;
+global using FinancialGoalsManager.Domain.Messages;

d50fc7c7b3a196bc0629561ef2da47f2462d8c8c
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-05-15 19:52:33 -0300

    feat: Adding new methods in FinancialGoal for add, remove and get transaction

--- FinancialGoalsManager.Domain/Entities/FinancialGoal.cs
+++ FinancialGoalsManager.Domain/Entities/FinancialGoal.cs
@@ -2,6 +2,8 @@
 
 public sealed class FinancialGoal : SoftDeleteEntity
 {
+    public const int TitleMaxLength = 200; 
+    
     public string Title { get; private set; } = null!;
     public decimal Goal { get; private set; }
     public DateTime DueDate { get; private set; }
@@ -21,10 +23,61 @@ public sealed class FinancialGoal : SoftDeleteEntity
         GoalStatus status
     )
     {
+        Guard.IsNotEmpty(title);
+        Guard.IsLessThanOrEqualTo(title.Length, TitleMaxLength, nameof(title));
+        Guard.IsNotDefault(goal);
+        Guard.IsNotDefault(dueDate);
+        
         Title = title;
         Goal = goal;
         DueDate = dueDate;
         MonthGoal = monthGoal;
         Status = status;
+    }
+    
+    public void Update(
+        string title,
+        decimal goal,
+        DateTime dueDate, 
+        decimal? monthGoal,
+        GoalStatus status
+    )
+    {
+        Guard.IsNotEmpty(title);
+        Guard.IsLessThanOrEqualTo(title.Length, TitleMaxLength, nameof(title));
+        Guard.IsNotDefault(goal);
+        Guard.IsNotDefault(dueDate);
+        
+        Title = title;
+        Goal = goal;
+        DueDate = dueDate;
+        MonthGoal = monthGoal;
+        Status = status;
+    }
+
+    public void AddTransaction(Transaction transaction)
+    {
+        if(transaction.Quantity < 0)
+            throw new ArgumentOutOfRangeException(ErrorMessages.HasToBeGreaterThan(nameof(transaction.Quantity), 0));
+        
+        if(transaction.Date.Date <= DateTime.Today)
+            throw new ArgumentOutOfRangeException(ErrorMessages.HasToBeGreaterThan(nameof(transaction.Date), DateTime.Today.Day));
+            
+        _transactions.Add(transaction);
+    }
+
+    public Transaction? GetTransaction(Guid transactionId)
+    {
+        return _transactions.FirstOrDefault(t => t.Id == transactionId);
+    }
+    
+    public void RemoveTransaction(Guid transactionId)
+    {
+        var transaction = GetTransaction(transactionId);
+
+        if (transaction is null)
+            throw new NotFoundException(ErrorMessages.NotFound<Transaction>());
+        
+        _transactions.Remove(transaction);
     }
 }

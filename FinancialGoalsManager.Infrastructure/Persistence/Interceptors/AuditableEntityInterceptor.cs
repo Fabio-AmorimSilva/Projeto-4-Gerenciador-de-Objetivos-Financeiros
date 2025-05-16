527f04360dbf01e53994af6c822930eb86609700
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-05-16 20:08:59 -0300

    feat: Adding AuditableEntityInterceptor for audit create info

---
+++ FinancialGoalsManager.Infrastructure/Persistence/Interceptors/AuditableEntityInterceptor.cs
@@ -0,0 +1,31 @@
+﻿namespace FinancialGoalsManager.Infrastructure.Persistence.Interceptors;
+
+public class AuditableEntityInterceptor : SaveChangesInterceptor
+{
+    public override int SavedChanges(SaveChangesCompletedEventData eventData, int result)
+    {
+        AuditCreateInfo(eventData.Context);
+        return base.SavedChanges(eventData, result);
+    }
+
+    public override ValueTask<int> SavedChangesAsync(SaveChangesCompletedEventData eventData, int result, CancellationToken cancellationToken = new CancellationToken())
+    {
+        AuditCreateInfo(eventData.Context);
+        return base.SavedChangesAsync(eventData, result, cancellationToken);
+    }
+
+    private void AuditCreateInfo(DbContext? context)
+    {
+        if (context is null)
+            return;
+        
+        var entities = context.ChangeTracker
+            .Entries<AuditableEntity>()
+            .Where(e => e.State == EntityState.Added);
+
+        foreach (var entity in entities)
+        {
+            entity.Entity.CreatedAt = DateTime.Now;
+        }    
+    }
+}

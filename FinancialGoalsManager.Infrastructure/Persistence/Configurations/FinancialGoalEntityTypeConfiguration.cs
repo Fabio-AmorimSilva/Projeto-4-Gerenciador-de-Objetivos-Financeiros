34fba4d0eef9e04409b6e907ab8e12099f7e1b1d
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-05-16 20:09:27 -0300

    feat: Adding entity type configurations of entities

---
+++ FinancialGoalsManager.Infrastructure/Persistence/Configurations/FinancialGoalEntityTypeConfiguration.cs
@@ -0,0 +1,43 @@
+﻿namespace FinancialGoalsManager.Infrastructure.Persistence.Configurations;
+
+public sealed class FinancialGoalEntityTypeConfiguration : IEntityTypeConfiguration<FinancialGoal>
+{
+    public void Configure(EntityTypeBuilder<FinancialGoal> builder)
+    {
+        builder
+            .ToTable("FinancialGoals");
+        
+        builder
+            .HasKey(fg => fg.Id);
+        
+        builder
+            .Property(fg => fg.Id)
+            .ValueGeneratedNever()
+            .IsRequired();
+
+        builder
+            .HasIndex(fg => fg.Id);
+
+        builder
+            .Property(fg => fg.Title)
+            .HasMaxLength(FinancialGoal.TitleMaxLength)
+            .IsRequired();
+
+        builder
+            .Property(fg => fg.Goal)
+            .HasColumnType("decimal(18,2)")
+            .IsRequired();
+
+        builder
+            .Property(fg => fg.DueDate)
+            .IsRequired();
+
+        builder
+            .Property(fg => fg.MonthGoal)
+            .IsRequired(false);
+
+        builder
+            .Property(fg => fg.Status)
+            .IsRequired();
+    }
+}
---
+++ FinancialGoalsManager.Infrastructure/Persistence/Configurations/TransactionEntityTypeConfiguration.cs
@@ -0,0 +1,33 @@
+﻿namespace FinancialGoalsManager.Infrastructure.Persistence.Configurations;
+
+public sealed class TransactionEntityTypeConfiguration : IEntityTypeConfiguration<Transaction>
+{
+    public void Configure(EntityTypeBuilder<Transaction> builder)
+    {
+        builder
+            .ToTable("Transactions");
+        
+        builder
+            .HasKey(t => t.Id);
+
+        builder
+            .Property(t => t.Id)
+            .ValueGeneratedNever()
+            .IsRequired();
+
+        builder
+            .HasIndex(t => t.Id);
+        
+        builder
+            .Property(t => t.Quantity)
+            .IsRequired();
+        
+        builder
+            .Property(t => t.Date)
+            .IsRequired();
+        
+        builder
+            .Property(t => t.Type)
+            .IsRequired();
+    }
+}

c2b595e038fe8ec1cbfea7dc70177acaf2d188e4
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-05-17 17:25:22 -0300

    feat: Creating ServiceCollectionExtensions for Infrastructure layer

---
+++ FinancialGoalsManager.Infrastructure/ServiceCollectionExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace Microsoft.Extensions.DependencyInjection;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+        services.AddDbContext<FinancialGoalManagerDbContext>(
+            options => options.UseSqlServer(connectionString: connectionString)
+        );
+
+        services.AddScoped<ISaveChangesInterceptor, AuditableEntityInterceptor>();
+        services.AddScoped<IFinancialGoalManagerDbContext>(provider => provider.GetRequiredService<FinancialGoalManagerDbContext>());
+        
+        return services;
+    }
+}

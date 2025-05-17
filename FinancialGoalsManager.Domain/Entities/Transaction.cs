9c8116e163da9db99e2fcabe6b2dc36e314dc6bb
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-05-17 17:24:17 -0300

    feat: Adding FinancialGoal and User navigation properties in Transaction entity

--- FinancialGoalsManager.Domain/Entities/Transaction.cs
+++ FinancialGoalsManager.Domain/Entities/Transaction.cs
@@ -5,17 +5,30 @@ public sealed class Transaction : SoftDeleteEntity
     public decimal Quantity { get; private set; }
     public TransactionType Type { get; private set; }
     public DateTime Date { get; private set; }
-
+    
+    public Guid FinancialGoalId { get; private set; }
+    public FinancialGoal FinancialGoal { get; private set; } = null!;
+    public Guid UserId { get; private set; }
+    public User User { get; private set; } = null!;
+    
     public Transaction(){}
     
     public Transaction(
         decimal quantity,
         TransactionType type,
-        DateTime date
+        DateTime date,
+        FinancialGoal financialGoal,
+        User user
     )
     {
         Quantity = quantity;
         Type = type;
         Date = date;
+        
+        FinancialGoal = financialGoal;
+        FinancialGoalId = financialGoal.Id;
+        
+        UserId = User.Id;
+        User = user;
     }
 }

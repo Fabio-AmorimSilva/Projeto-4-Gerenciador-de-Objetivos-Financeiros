f9f6864c8a56829c4d0efa6b5b16565e4c3c7c94
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-05-17 17:17:56 -0300

    feat: Adding entity user and entity type configuration

---
+++ FinancialGoalsManager.Domain/Entities/User.cs
@@ -0,0 +1,90 @@
+﻿namespace FinancialGoalsManager.Domain.Entities;
+
+public class User : SoftDeleteEntity
+{
+    public const int MaxNameLength = 150;
+
+    public string Name { get; private set; }
+    public string Email { get; private set; }
+    public string Password { get; private set; }
+    
+    private readonly List<FinancialGoal> _financialGoals = [];
+    public IReadOnlyCollection<FinancialGoal> FinancialGoals => _financialGoals;
+    
+    private readonly List<Transaction> _transactions = [];
+    public IReadOnlyCollection<Transaction> Transactions => _transactions;
+    
+    protected User(){}
+    
+    public User(
+        string name,
+        string email,
+        string password
+    )
+    {
+        Guard.IsNotWhiteSpace(name);
+        Guard.IsLessThanOrEqualTo(name.Length, MaxNameLength, nameof(name));
+        Guard.IsNotWhiteSpace(email);
+        Guard.IsNotWhiteSpace(password);
+
+        Name = name;
+        Email = email;
+        Password = password;
+    }
+
+    public void Update(
+        string name,
+        string email
+    )
+    {
+        Guard.IsNotWhiteSpace(name);
+        Guard.IsLessThanOrEqualTo(name.Length, MaxNameLength, nameof(name));
+        Guard.IsNotWhiteSpace(email);
+
+        Name = name;
+        Email = email;
+    }
+
+    public void AddGoal(FinancialGoal financialGoal)
+    {
+        _financialGoals.Add(financialGoal);
+    }
+
+    public void RemoveGoal(FinancialGoal financialGoal)
+    {
+        _financialGoals.Remove(financialGoal);
+    }
+    
+    public void AddTransaction(Transaction transaction)
+    {
+        if(transaction.Quantity < 0)
+            throw new ArgumentOutOfRangeException(ErrorMessages.HasToBeGreaterThan(nameof(transaction.Quantity), 0));
+        
+        if(transaction.Date.Date <= DateTime.Today)
+            throw new ArgumentOutOfRangeException(ErrorMessages.HasToBeGreaterThan(nameof(transaction.Date), DateTime.Today.Day));
+            
+        _transactions.Add(transaction);
+    }
+
+    public Transaction? GetTransaction(Guid transactionId)
+    {
+        return _transactions.FirstOrDefault(t => t.Id == transactionId);
+    }
+    
+    public void RemoveTransaction(Guid transactionId)
+    {
+        var transaction = GetTransaction(transactionId);
+
+        if (transaction is null)
+            throw new NotFoundException(ErrorMessages.NotFound<Transaction>());
+        
+        _transactions.Remove(transaction);
+    }
+    
+    public void UpdatePassword(string password)
+    {
+        Guard.IsNotWhiteSpace(password);
+        
+        Password = password;
+    }
+}
---
+++ FinancialGoalsManager.Infrastructure/Persistence/Configurations/UserEntityTypeConfiguration.cs
@@ -0,0 +1,34 @@
+﻿namespace FinancialGoalsManager.Infrastructure.Persistence.Configurations;
+
+public sealed class UserEntityTypeConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder
+            .ToTable("Users");
+        
+        builder
+            .HasKey(u => u.Id);
+
+        builder
+            .HasIndex(u => u.Id);
+        
+        builder
+            .Property(u => u.Id)
+            .ValueGeneratedNever()
+            .IsRequired();
+
+        builder
+            .Property(u => u.Name)
+            .HasMaxLength(User.MaxNameLength)
+            .IsRequired();
+        
+        builder
+            .Property(u => u.Email)
+            .IsRequired();
+
+        builder
+            .Property(u => u.Password)
+            .IsRequired();
+    }
+}

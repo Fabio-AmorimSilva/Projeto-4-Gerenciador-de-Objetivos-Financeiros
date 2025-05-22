eecc1d34a0fa7851b3bcb26698834d67da86872c
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-05-22 20:02:42 -0300

    feat: Creating action filter

---
+++ FinancialGoalsManager.Api/Filters/ModelStateValidatorFilter.cs
@@ -0,0 +1,27 @@
+﻿namespace FinancialGoalsManager.Api.Filters;
+
+public sealed class ModelStateValidatorFilter : IAsyncActionFilter
+{
+    public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+    {
+        if (!context.ModelState.IsValid)
+        {
+            var errors = context.ModelState
+                .SelectMany(modelState =>
+                    modelState.Value?.Errors.Select(e => e.ErrorMessage) ??
+                    Array.Empty<string>()
+                );
+
+            var error = new Error
+            {
+                StatusCode = 422,
+                Message = errors.First()
+            };
+
+            context.HttpContext.Response.StatusCode = error.StatusCode;
+            await context.HttpContext.Response.WriteAsJsonAsync(error);
+            
+            await next();
+        }
+    }
+}
---
+++ FinancialGoalsManager.Api/ServiceCollectionExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace Microsoft.Extensions.DependencyInjection;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddApi(this IServiceCollection services)
+    {
+        services.AddMvc(options => options.Filters.Add<ModelStateValidatorFilter>());
+
+        return services;
+    }
+}

c7f55a575046a71f90bf1b1a4c5c0261120c68c2
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-05-22 20:04:31 -0300

    feat: Creating new EmailAlreadyExistsSpec specification

---
+++ FinancialGoalsManager.Domain/Specifications/EmailAlreadyExistsSpec.cs
@@ -0,0 +1,9 @@
+﻿namespace FinancialGoalsManager.Domain.Specifications;
+
+public class EmailAlreadyExistsSpec : Specification<User>
+{
+    public EmailAlreadyExistsSpec(Guid userId, string email)
+    {
+        Query.Where(u => u.Id != userId && u.Email == email);
+    }
+}

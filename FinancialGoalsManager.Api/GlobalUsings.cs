07e0aedc11724023b994d8c50039621729164931
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-05-23 18:36:51 -0300

    chore: Adding namespaces

--- FinancialGoalsManager.Api/GlobalUsings.cs
+++ FinancialGoalsManager.Api/GlobalUsings.cs
@@ -1,3 +1,5 @@
 global using FinancialGoalsManager.Api.Filters;
 global using FinancialGoalsManager.Api.Filters.Models;
+global using FluentValidation;
+global using Microsoft.AspNetCore.Mvc;
 global using Microsoft.AspNetCore.Mvc.Filters;

14d885f5e8c219c4b262f0a4a2a991ab8bc7c7ea
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-05-25 17:29:28 -0300

    feat: Creating UseCaseResult

---
+++ FinancialGoalsManager.Application/Common/Models/UseCaseResult.cs
@@ -0,0 +1,43 @@
+﻿namespace FinancialGoalsManager.Application.Common.Models;
+
+public class UseCaseResult
+{
+    public bool IsSuccess { get; set; }
+    public string Message { get; set; }
+
+    protected UseCaseResult()
+    {
+    }
+
+    public static UseCaseResult Success()
+        => new()
+        {
+            IsSuccess = true
+        };
+
+    public static UseCaseResult Error(string message)
+        => new UseCaseResult()
+        {
+            IsSuccess = false,
+            Message = message
+        };
+}
+
+public class UseCaseResult<T> : UseCaseResult
+{
+    public T? Data { get; set; }
+
+    public static UseCaseResult<T> Success(T? data)
+        => new()
+        {
+            IsSuccess = true,
+            Data = data
+        };
+
+    public new static UseCaseResult<T> Error(string message)
+        => new()
+        {
+            IsSuccess = false,
+            Message = message
+        };
+}

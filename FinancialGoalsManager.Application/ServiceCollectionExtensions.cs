a15c79eb0e35448bfa0497ecf8bce8d401b0b7cb
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-05-25 17:29:49 -0300

    feat: Creating ServiceCollectionExtensions for Application layer

---
+++ FinancialGoalsManager.Application/ServiceCollectionExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace Microsoft.Extensions.DependencyInjection;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddScoped<IAddUserUseCase, AddUserUseCase>();
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+        
+        return services;
+    }
+}

90ccef725398ea7decc23b91197f229bd17e4b6d
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-05-25 17:29:12 -0300

    feat: Creating AddUserInputModel

---
+++ FinancialGoalsManager.Application/UseCases/Users/AddUser/AddUserInputModel.cs
@@ -0,0 +1,26 @@
+﻿namespace FinancialGoalsManager.Application.UseCases.Users.AddUser;
+
+public sealed record AddUserInputModel
+{
+    public required string Name { get; init; }
+    public required string Email { get; init; }
+    public required string Password { get; init; }
+}
+
+public class AddUserInputModelValidator : AbstractValidator<AddUserInputModel>
+{
+    public AddUserInputModelValidator()
+    {
+        RuleFor(m => m.Name)
+            .NotEmpty()
+            .WithMessage(ErrorMessages.CannotBeEmpty(nameof(AddUserInputModel.Name)));
+        
+        RuleFor(m => m.Email)
+            .NotEmpty()
+            .WithMessage(ErrorMessages.CannotBeEmpty(nameof(AddUserInputModel.Email)));
+        
+        RuleFor(m => m.Password)
+            .NotEmpty()
+            .WithMessage(ErrorMessages.CannotBeEmpty(nameof(AddUserInputModel.Password)));
+    }
+}

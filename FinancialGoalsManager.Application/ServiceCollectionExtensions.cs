45bca401edf1f0ca9edb9782b8ae6d81ee6a76e8
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-05-27 19:48:19 -0300

    feat: Adding new UseCase

--- FinancialGoalsManager.Application/ServiceCollectionExtensions.cs
+++ FinancialGoalsManager.Application/ServiceCollectionExtensions.cs
@@ -5,6 +5,9 @@ public static class ServiceCollectionExtensions
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
         services.AddScoped<IAddUserUseCase, AddUserUseCase>();
+        services.AddScoped<IUpdateUserUseCase, UpdateUserUseCase>();
+        services.AddScoped<IUpdatePasswordUseCase, UpdatePasswordUseCase>();
+        services.AddScoped<IDeleteUserUseCase, DeleteUserUseCase>();
         services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
         
         return services;

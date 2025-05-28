f31d4652803e47923a0d1b5da02c8132bc3855ba
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-05-28 19:39:31 -0300

    feat: Creating UserService

---
+++ FinancialGoalsManager.Application/Services/UserService.cs
@@ -0,0 +1,11 @@
+﻿namespace FinancialGoalsManager.Application.Services;
+
+public sealed class UserService(IHttpContextAccessor acessor)
+{
+    public string GetLoggedUserId()
+    {
+        var userId = acessor.HttpContext.User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Sid)?.Value;
+
+        return userId ?? string.Empty;
+    }
+}

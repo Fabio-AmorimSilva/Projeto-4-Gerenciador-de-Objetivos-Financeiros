9132cfeb2d1836439ac4a0d8e4458db5c6bca6b1
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-05-29 20:02:25 -0300

    feat: Adding Jwt Infrastructure

--- FinancialGoalsManager.Infrastructure/ServiceCollectionExtensions.cs
+++ FinancialGoalsManager.Infrastructure/ServiceCollectionExtensions.cs
@@ -8,12 +8,43 @@ public static class ServiceCollectionExtensions
         services.AddDbContext<FinancialGoalManagerDbContext>(
             options => options.UseSqlServer(connectionString: connectionString)
         );
-
+        
+        services.AddJwtConfig(configuration);
         services.AddScoped<ISaveChangesInterceptor, AuditableEntityInterceptor>();
         services.AddScoped<IPasswordHashService, PasswordHashService>();
         services.AddScoped<ITokenService, TokenService>();
         services.AddScoped<IFinancialGoalManagerDbContext>(provider => provider.GetRequiredService<FinancialGoalManagerDbContext>());
         
+        return services;
+    }
+
+    private static IServiceCollection AddJwtConfig(this IServiceCollection services, IConfiguration configuration)
+    {
+        var settings = configuration.GetSection("JwtSettings");
+        services.Configure<JwtSettings>(settings);
+        
+        var appSettings = settings.Get<JwtSettings>();
+        var key = Encoding.ASCII.GetBytes(appSettings?.JwtKey!);
+
+        services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        }).AddJwtBearer(options =>
+        {
+            options.RequireHttpsMetadata = false;
+            options.SaveToken = true;
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(key),
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidAudience = appSettings?.ValidOn,
+                ValidIssuer = appSettings?.Emissary
+            };
+        });
+
         return services;
     }
 }

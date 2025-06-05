702cadfa386559509e67a9eff7a7ff1c20714bcb
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-06-05 19:54:04 -0300

    feat: Adding RequestContextService and HttpContextAccessor to IoC

--- FinancialGoalsManager.Api/ServiceCollectionExtensions.cs
+++ FinancialGoalsManager.Api/ServiceCollectionExtensions.cs
@@ -5,6 +5,8 @@ public static class ServiceCollectionExtensions
     public static IServiceCollection AddApi(this IServiceCollection services)
     {
         services.AddMvc(options => options.Filters.Add<ModelStateValidatorFilter>());
+        services.AddHttpContextAccessor();
+        services.AddScoped<IRequestContextService, RequestContextService>();
 
         return services;
     }

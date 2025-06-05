0d4f0f995b1584730f30bc4a805b5403f7736c84
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-06-05 19:53:21 -0300

    feat: Creating RequestContextService

---
+++ FinancialGoalsManager.Api/Services/RequestContextService.cs
@@ -0,0 +1,6 @@
+﻿namespace FinancialGoalsManager.Api.Services;
+
+public sealed class RequestContextService(IHttpContextAccessor httpContextAccessor) : IRequestContextService
+{
+    public Guid UserId => new(httpContextAccessor.HttpContext?.User?.FindFirstValue(ClaimTypes.Sid) ?? string.Empty);
+}
---
+++ FinancialGoalsManager.Application/Common/Interfaces/IRequestContextService.cs
@@ -0,0 +1,6 @@
+﻿namespace FinancialGoalsManager.Application.Common.Interfaces;
+
+public interface IRequestContextService
+{
+    Guid UserId { get; }
+}

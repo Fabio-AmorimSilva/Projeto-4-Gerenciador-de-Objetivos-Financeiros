c1f3b6afe1393e57f98cff35f78bc10a9ca64679
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-06-08 17:53:34 -0300

    feat: Adding validator in DeleteFinancialGoalUseCaseInputModel

--- FinancialGoalsManager.Application/UseCases/FinancialGoals/DeleteFinancialGoal/DeleteFinancialGoalUseCaseInputModel.cs
+++ FinancialGoalsManager.Application/UseCases/FinancialGoals/DeleteFinancialGoal/DeleteFinancialGoalUseCaseInputModel.cs
@@ -4,3 +4,13 @@ public sealed record DeleteFinancialGoalUseCaseInputModel
 {
     public Guid FinancialGoalId { get; set; }
 }
+
+public class DeleteFinancialGoalUseCaseInputModelValidator : AbstractValidator<DeleteFinancialGoalUseCaseInputModel>
+{
+    public DeleteFinancialGoalUseCaseInputModelValidator()
+    {
+        RuleFor(uc => uc.FinancialGoalId)
+            .NotEmpty()
+            .WithMessage(ErrorMessages.CannotBeEmpty(nameof(DeleteFinancialGoalUseCaseInputModel.FinancialGoalId)));
+    }
+}

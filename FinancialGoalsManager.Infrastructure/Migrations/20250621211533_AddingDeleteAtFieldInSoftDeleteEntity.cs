8bd89130e362df311b74161d51befaec88e91974
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-06-21 18:16:01 -0300

    feat: Adding new migration

---
+++ FinancialGoalsManager.Infrastructure/Migrations/20250621211533_AddingDeleteAtFieldInSoftDeleteEntity.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FinancialGoalsManager.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddingDeleteAtFieldInSoftDeleteEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeleteAt",
+                table: "Users",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeleteAt",
+                table: "Transactions",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeleteAt",
+                table: "FinancialGoals",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DeleteAt",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "DeleteAt",
+                table: "Transactions");
+
+            migrationBuilder.DropColumn(
+                name: "DeleteAt",
+                table: "FinancialGoals");
+        }
+    }
+}

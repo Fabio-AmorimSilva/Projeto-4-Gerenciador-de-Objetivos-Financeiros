822a61835c20ed59f4430a82d4f53f575a71e740
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-06-21 18:16:29 -0300

    feat: Creating SoftDeleteEntityInterceptor

---
+++ FinancialGoalsManager.Infrastructure/Persistence/Interceptors/SoftDeleteEntityInterceptor.cs
@@ -0,0 +1,33 @@
+﻿namespace FinancialGoalsManager.Infrastructure.Persistence.Interceptors;
+
+public sealed class SoftDeleteEntityInterceptor : SaveChangesInterceptor
+{
+    public override InterceptionResult<int> SavingChanges(DbContextEventData eventData, InterceptionResult<int> result)
+    {
+        AuditSoftDeleteInfo(eventData.Context);
+        return base.SavingChanges(eventData, result);
+    }
+
+    public override ValueTask<InterceptionResult<int>> SavingChangesAsync(DbContextEventData eventData, InterceptionResult<int> result, CancellationToken cancellationToken = new CancellationToken())
+    {
+        AuditSoftDeleteInfo(eventData.Context);
+        return base.SavingChangesAsync(eventData, result, cancellationToken);
+    }
+
+    private void AuditSoftDeleteInfo(DbContext? context)
+    {
+        if (context is null)
+            return;
+
+        var entities = context.ChangeTracker
+            .Entries<SoftDeleteEntity>()
+            .Where(e => e.State == EntityState.Deleted);
+
+        foreach (var entity in entities)
+        {
+            entity.State = EntityState.Modified;
+            entity.Entity.IsDeleted = true;
+            entity.Entity.DeleteAt = DateTime.Now;
+        }
+    }
+}

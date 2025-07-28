26adc805730935a8045fa00a8009e7e302980275
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-07-28 16:58:30 -0300

    chore: Deleting unused classes

--- FinancialGoalsManager.Infrastructure/EventBusRabbitMq/EventBusOptions.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace FinancialGoalsManager.Infrastructure.EventBusRabbitMq;
-
-public class EventBusOptions
-{
-    public string SubscriptionClientName { get; set; }
-    public int RetryCount { get; set; } = 10;
-}

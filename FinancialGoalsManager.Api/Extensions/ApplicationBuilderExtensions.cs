8e389399fdddf685b98b5865a1447530e913f4c9
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-07-31 19:48:15 -0300

    feat: Creating ApplicationBuilderExtensions

---
+++ FinancialGoalsManager.Api/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace Microsoft.AspNetCore.Builder;
+
+public static class ApplicationBuilderExtensions
+{
+    public static void ConfigureEventBusHandlers(this IApplicationBuilder app)
+    {
+        var eventBus = app.ApplicationServices.GetRequiredService<IEventBus>();
+
+        eventBus.Subscribe<TransactionCreatedIntegrationEvent, TransactionCreatedIntegrationEventHandler>();
+        eventBus.Subscribe<TransactionDeletedIntegrationEvent, TransactionDeletedIntegrationEventHandler>();
+        eventBus.Subscribe<FinancialGoalCreatedIntegrationEvent, FinancialGoalCreatedIntegrationEventHandler>();
+        eventBus.Subscribe<FinancialGoalDeletedIntegrationEvent, FinancialGoalDeletedIntegrationEventHandler>();
+        eventBus.Subscribe<FinancialGoalUpdatedIntegrationEvent, FinancialGoalUpdatedIntegrationEventHandler>();
+    }
+}

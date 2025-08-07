b85d9821b148aea64a436355dc89f0699facc20a
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-08-07 19:59:08 -0300

    feat: Creating DeleteTransactionUseCaseInputModel

---
+++ FinancialGoalsManager.Application/UseCases/Transactions/DeleteTransaction/DeleteTransactionUseCaseInputModel.cs
@@ -0,0 +1,16 @@
+﻿namespace FinancialGoalsManager.Application.UseCases.Transactions.DeleteTransaction;
+
+public sealed record DeleteTransactionUseCaseInputModel
+{
+    public Guid TransactionId { get; set; }
+}
+
+public class DeleteTransactionUseCaseInputModelValidator : AbstractValidator<DeleteTransactionUseCaseInputModel>
+{
+    public DeleteTransactionUseCaseInputModelValidator()
+    {
+        RuleFor(useCase => useCase.TransactionId)
+            .NotEmpty()
+            .WithMessage(ErrorMessages.CannotBeEmpty(nameof(DeleteTransactionUseCaseInputModel.TransactionId)));
+    }
+}

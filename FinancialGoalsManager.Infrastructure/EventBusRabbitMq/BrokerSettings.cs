41e93dd4892cef6c94c32593cf1f2935bbdb099b
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-08-07 19:58:08 -0300

    feat: Creating BrokerSettings

---
+++ FinancialGoalsManager.Infrastructure/EventBusRabbitMq/BrokerSettings.cs
@@ -0,0 +1,12 @@
+﻿namespace FinancialGoalsManager.Infrastructure.EventBusRabbitMq;
+
+public sealed record BrokerSettings
+{
+    public string Host { get; init; } = null!;
+    public string Uri { get; init; } = null!;
+    public int Port { get; init; }
+    public string UserName { get; init; } = null!;
+    public string Password { get; init; } = null!;
+    public string BrokerName { get; init; } = null!;
+    public string QueueName { get; init; } = null!;
+}

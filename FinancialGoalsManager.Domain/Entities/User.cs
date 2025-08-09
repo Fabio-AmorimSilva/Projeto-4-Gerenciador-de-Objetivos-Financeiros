fb54534d0c0bbce2c7c43f30a113b8839e54c0b0
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-08-09 15:59:44 -0300

    style: Code formatting

--- FinancialGoalsManager.Domain/Entities/User.cs
+++ FinancialGoalsManager.Domain/Entities/User.cs
@@ -7,15 +7,17 @@ public class User : SoftDeleteEntity
     public string Name { get; private set; }
     public string Email { get; private set; }
     public string Password { get; private set; }
-    
+
     private readonly List<FinancialGoal> _financialGoals = [];
     public IReadOnlyCollection<FinancialGoal> FinancialGoals => _financialGoals;
-    
+
     private readonly List<Transaction> _transactions = [];
     public IReadOnlyCollection<Transaction> Transactions => _transactions;
-    
-    protected User(){}
-    
+
+    protected User()
+    {
+    }
+
     public User(
         string name,
         string email,
@@ -54,28 +56,31 @@ public class User : SoftDeleteEntity
         Guid financialGoalId,
         string title,
         decimal goal,
-        DateTime dueDate, 
+        DateTime dueDate,
         decimal? monthGoal
     )
     {
         var financialGoal = GetGoal(financialGoalId);
         if (financialGoal is null)
             return UseCaseResult<FinancialGoal>.Error(ErrorMessages.NotFound<FinancialGoal>());
-        
+
         financialGoal.Update(
             title: title,
             goal: goal,
             dueDate: dueDate,
             monthGoal: monthGoal
         );
-        
+
         return UseCaseResult<FinancialGoal>.Success(financialGoal);
     }
 
-    public FinancialGoal? GetGoal(Guid financialGoalId)
+    public FinancialGoal GetGoal(Guid financialGoalId)
     {
         var financialGoal = _financialGoals.FirstOrDefault(f => f.Id == financialGoalId);
 
+        if (financialGoal is null)
+            throw new NotFoundException(ErrorMessages.NotFound<FinancialGoal>());
+        
         return financialGoal;
     }
 
@@ -83,19 +88,19 @@ public class User : SoftDeleteEntity
     {
         _financialGoals.Remove(financialGoal);
     }
-    
+
     public void AddTransaction(Transaction transaction)
     {
-        if(transaction.Quantity < 0)
+        if (transaction.Quantity < 0)
             throw new ArgumentOutOfRangeException(ErrorMessages.HasToBeGreaterThan(nameof(transaction.Quantity), 0));
-        
-        if(transaction.Date.Date <= DateTime.Today)
+
+        if (transaction.Date.Date <= DateTime.Today)
             throw new ArgumentOutOfRangeException(ErrorMessages.HasToBeGreaterThan(nameof(transaction.Date), DateTime.Today.Day));
-            
+
         var financialGoal = GetGoal(transaction.FinancialGoalId);
         
         financialGoal.AddTotal(transaction.Quantity);
-        
+
         _transactions.Add(transaction);
     }
 
@@ -103,7 +108,7 @@ public class User : SoftDeleteEntity
     {
         return _transactions.FirstOrDefault(t => t.Id == transactionId);
     }
-    
+
     public void DeleteTransaction(Transaction transaction)
     {
         var financialGoal = GetGoal(transaction.FinancialGoalId);
@@ -112,11 +117,11 @@ public class User : SoftDeleteEntity
 
         _transactions.Remove(transaction);
     }
-    
+
     public void UpdatePassword(string password)
     {
         Guard.IsNotWhiteSpace(password);
-        
+
         Password = password;
     }
 }
